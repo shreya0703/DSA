bd353afc7cdb9b1a96e18403fe35a8a9815e6493
shreya0703 <147245183+shreya0703@users.noreply.github.com>
2025-05-08 22:16:33 +0530

    Time: 1 ms (83.87%) | Memory: 40.7 MB (85.10%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -3,11 +3,16 @@ class Solution {
         int revNum = 0;
         while( x!=0){
             int last_digit = x%10;
-             if (revNum > 214748364 || revNum < -214748364) return 0;
+             
 
             x = x/10;
+            if (revNum > Integer.MAX_VALUE / 10 || (revNum == Integer.MAX_VALUE / 10 && last_digit > 7))
+                return 0;
+            if (revNum < Integer.MIN_VALUE / 10 || (revNum == Integer.MIN_VALUE / 10 && last_digit < -8))
+                return 0;
             revNum =(revNum*10)+last_digit;
             
+           
         }
         return revNum;
     }
